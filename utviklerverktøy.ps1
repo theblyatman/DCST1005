8cb50df402a30879dca14800ae455d60f056e98e
Nikita Ryazhskikh <nikitar@stud.ntnu.no>
2026-01-13 14:38:55 +0100

    programmer og oppdateringer

---
+++ utviklerverktøy.ps1
@@ -0,0 +1,10 @@
+choco install nodejs -y              # Node.js
+choco install python -y              # Python
+choco install dotnet-sdk -y          # .NET SDK
+choco install azure-cli -y           # Azure CLI
+choco install terraform -y           # Terraform
+choco install 7zip -y                # 7-Zip
+choco install notepadplusplus -y     # Notepad++
+choco install putty -y               # PuTTY
+choco install winscp -y              # WinSCP
+choco install postman -y             # Postman (API testing)
