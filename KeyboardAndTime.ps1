2e9f39e8a0b4749b859e79c7493868dcc85af886
nikitita <rjnikita14@gmail.coml>
2026-02-05 13:50:50 +0100

    Fikse keyboard

--- KeyboardAndTime.ps1
+++ KeyboardAndTime.ps1
@@ -1,10 +1,10 @@
-time=(Get-TimeZone).id
-keyboard=(Get-WinUserLanguageList).LanguageTag
+$time = (Get-TimeZone).Id
+$languages = Get-WinUserLanguageList
 
 if ($time -ne "W. Europe Standard Time") {
     Set-TimeZone -Id "W. Europe Standard Time"
 }
 
-if ($keyboard -notcontains "nb-NO") {
-    Set-WinUserLanguageList "nb-NO" -Force
+if ($languages.LanguageTag -notcontains "nb-NO") {
+    Set-WinUserLanguageList -LanguageList "nb-NO" -Force
 }
